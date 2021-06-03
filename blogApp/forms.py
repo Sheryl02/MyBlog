067d1d2bd34f33ec07d35f68441b51386ae767e8
Sheryl02 <85014905+Sheryl02@users.noreply.github.com>
2021-06-03 12:34:21 +0530

    Update forms.py

--- blogApp/forms.py
+++ blogApp/forms.py
@@ -22,26 +22,6 @@ class addPostForm(forms.ModelForm):
             'body':forms.Textarea(attrs={'class':'form-control'}),
         }
 
-# class postForm(forms.ModelForm):
-#     title = forms.CharField(max_length=100, widget = forms.TextInput(attrs={'class':'form-control'}))
-#     author= forms.CharField(max_length=100, widget = forms.TextInput(attrs={'class':'form-control'})),
-#     body = forms.CharField(max_length=100, widget = forms.Textarea(attrs={'class':'form-control'})),
-#     category = forms.ChoiceField(choices = choice_list)
-#
-#     class Meta:
-#         model = Post
-#         fields = ['title', 'author','body','category']
-#         # fields = ['title', 'author','body']
-#
-#         # widgets = {
-#         #     'title':forms.TextInput(attrs={'class':'form-control'}),
-#         #     'author':forms.TextInput(attrs={'class':'form-control'}),
-#         #     'body':forms.Textarea(attrs={'class':'form-control'}),
-#         #     'category':forms.Select(choices = choice_list, attrs={'class':'from-control'})
-#         #     # 'category':forms.Select()
-#         #
-#         # }
-
 class editForm(forms.ModelForm):
     class Meta:
         model = Post
